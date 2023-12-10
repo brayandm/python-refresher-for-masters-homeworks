d8c0f02e0b3a56292ba736f372f388e646722342
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-12-11 00:40:20 +0100

    update

--- BigHomeWork2/experiments/classifiers/exp_hw6_2.py
+++ BigHomeWork2/experiments/classifiers/exp_hw6_2.py
@@ -9,18 +9,31 @@ from importlib import reload
 import experiments.transformers.np_vectorizer
 reload(experiments.transformers.np_vectorizer)
 
+from sklearn.preprocessing import FunctionTransformer
+
 class ExpHW6Classifier2(BaseEstimator, ClassifierMixin):
 
     def __init__(self, n_components=2, extra_features=[]):
-        self.pipeline = Pipeline([
+        self.transformer = Pipeline([
             ('np_vectorizer', experiments.transformers.np_vectorizer.NPVectorizer()),
             ('truncated_svd', TruncatedSVD(n_components=n_components)),
+        ])
+
+        def append_extra_features(X, extra_features):
+            return np.hstack((X, extra_features)) if extra_features is not None else X
+
+        self.classifier = Pipeline([
+            ('feature_combiner', FunctionTransformer(append_extra_features, validate=False)),
             ('logistic_regression', LogisticRegression())
         ])
 
-    def fit(self, dataset, y=None):
-        self.pipeline.fit(dataset, y)
+    def fit(self, dataset, y=None, extra_features=None):
+        X_transformed = self.transformer.fit_transform(dataset)
+        self.classifier.set_params(feature_combiner__kw_args={'extra_features': extra_features})
+        self.classifier.fit(X_transformed, y)
         return self
 
-    def predict(self, dataset):
-        return self.pipeline.predict(dataset)
+    def predict(self, dataset, extra_features=None):
+        X_transformed = self.transformer.transform(dataset)
+        self.classifier.set_params(feature_combiner__kw_args={'extra_features': extra_features})
+        return self.classifier.predict(X_transformed)
