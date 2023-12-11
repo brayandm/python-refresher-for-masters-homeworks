e94bb7f3bd8d6358f5a95418f1c32da534553e66
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-12-11 01:43:41 +0100

    update

--- BigHomeWork2/experiments/classifiers/exp_hw6_17.py
+++ BigHomeWork2/experiments/classifiers/exp_hw6_17.py
@@ -11,51 +11,57 @@ reload(experiments.transformers.word_vectorizer_1gram)
 
 from sklearn.preprocessing import FunctionTransformer
 
+import numpy as np
+
 class ExpHW6Classifier17(BaseEstimator, ClassifierMixin):
 
     def __init__(self, n_components=2, extra_features=[], n_estimators=100, learning_rate=0.05):
 
         self.transformer = {}
 
-        transformer[0] = Pipeline([
+        self.transformer[0] = Pipeline([
             ('word_vectorizer_1gram', experiments.transformers.word_vectorizer_1gram.WordVectorizer1Gram()),
             ('truncated_svd', TruncatedSVD(n_components=10)),
         ])
 
-        transformer[1] = Pipeline([
+        self.transformer[1] = Pipeline([
             ('np_vectorizer', experiments.transformers.np_vectorizer.NPVectorizer()),
             ('truncated_svd', TruncatedSVD(n_components=10)),
         ])
-        transformer[2] = Pipeline([
+
+        self.transformer[2] = Pipeline([
             ('word_vectorizer_2gram', experiments.transformers.word_vectorizer_2gram.WordVectorizer2Gram()),
             ('truncated_svd', TruncatedSVD(n_components=10)),
         ])
 
-        transformer[3] = Pipeline([
+        self.transformer[3] = Pipeline([
             ('word_vectorizer_2gram_by_character', experiments.transformers.word_vectorizer_2gram_by_character.WordVectorizer2GramByCharacter()),
             ('truncated_svd', TruncatedSVD(n_components=10)),
         ])
 
-        transformer[4] = Pipeline([
+        self.transformer[4] = Pipeline([
             ('polynomial_features_by_character', experiments.transformers.polynomial_features_by_character.PolynomialFeaturesByCharacter()),
             ('truncated_svd', TruncatedSVD(n_components=10)),
         ])
 
-        self.classifier = LogisticRegression()
+        self.classifier = XGBClassifier(
+            n_estimators=n_estimators,
+            learning_rate=learning_rate
+        )
 
     def fit(self, dataset, y=None, extra_features=None):
         concated_results = None
 
         for i in range(5):
 
-            result = transformer[i].fit_transform(train_df)
+            result = self.transformer[i].fit_transform(dataset)
 
             if concated_results is None:
                 concated_results = result
             else:
                 concated_results = np.hstack((concated_results, result))
                 
-        classifier.fit(concated_results, train_df['Target'])
+        self.classifier.fit(concated_results, dataset['Target'])
 
         return self
 
@@ -64,12 +70,12 @@ class ExpHW6Classifier17(BaseEstimator, ClassifierMixin):
 
         for i in range(5):
 
-            result = transformer[i].fit_transform(train_df)
+            result = self.transformer[i].fit_transform(dataset)
 
             if concated_results is None:
                 concated_results = result
             else:
                 concated_results = np.hstack((concated_results, result))
-        classifier.fit(concated_results, train_df['Target'])
+        self.classifier.fit(concated_results, dataset['Target'])
 
-        return classifier.predict(concated_results)
+        return self.classifier.predict(concated_results)
